47308708677f7094e254aad9e1b245e7ce98606d
Michael Mueller <michael.mueller@hm.edu>
2021-08-05 15:41:39 +0200

    home: progress

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,19 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {ConfigContainer} from "../../model/ConfigContainer";
+import {ConfigDataService} from "../../services/config-data.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+    configContainers: ConfigContainer[] = [];
+    filteredConfigContainer: ConfigContainer[] = [];
 
-  ngOnInit(): void {
-  }
+    searchValue = '';
+    isLoading = true;
+    isError = false;
+
+    constructor(private router: Router,
+                private configDataService: ConfigDataService) {
+    }
+
+    ngOnInit(): void {
+        this.configDataService
+            .getConfigs()
+            .subscribe(data => {
+                this.configContainers = data;
+                this.filteredConfigContainer = data;
+                this.isLoading = false;
+            }, error => {
+                this.isLoading = false;
+                this.isError = true;
+            });
+    }
+
+    filterConfigs() {
+        if (!this.searchValue) {
+            this.filteredConfigContainer = this.configContainers;
+            return;
+        }
+        this.filteredConfigContainer = this.configContainers.filter(configContainer => {
+                return (configContainer.deviceName?.includes(this.searchValue)
+                    || configContainer.deviceBrand?.includes(this.searchValue)
+                    || configContainer.configId?.includes(this.searchValue));
+            }
+        );
+    }
 
     routeTo(path: string) {
-      this.router.navigate([`/${path}`]);
+        this.router.navigate([`/${path}`]);
     }
 }
